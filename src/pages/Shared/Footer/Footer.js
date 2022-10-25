8308f63a4e1152d1b93f97d76ea2881e8b8b8e6a
unknown <mosharafctg111@gmail.com>
2022-10-26 02:21:08 +0600

    added the footer

--- src/pages/Shared/Footer/Footer.js
+++ src/pages/Shared/Footer/Footer.js
@@ -2,8 +2,10 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <div>
-            <h1>this is footer</h1>
+        <div className='p-5 bg-dark'>
+            <div className='p-5'>
+                <h4 className='text-white text-center'>©Copyright 2022 || All rights Reserved by <span className='text-danger'>Mosharaf Hossain</span> </h4>
+            </div>
         </div>
     );
 };
